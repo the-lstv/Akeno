8b1a5b493f92194081919e07cf6b3603ac00aff5
LSTV <lstv@mail.com>
2024-06-15 17:40:14 +0200

    fix

--- app.js
+++ app.js
@@ -421,7 +421,6 @@ function build(){
             "Access-Control-Allow-Credentials": "true",
             "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
             "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization",
-            ...headers,
         })
 
         res.send = (message, headers = {}, status) => {
