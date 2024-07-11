ae3bb174eccba990289ccd2e16d3bfa69de6f93d
LSTV <lstv@mail.com>
2024-07-11 21:22:06 +0200

    fix account creation crash

--- api/v2.js
+++ api/v2.js
@@ -500,41 +500,45 @@ main = {
                         if(!req.secured) return error(35);
                         if(req.method != "POST") return error(30);
                         
-                        req.parseBody((data, fail) => {
-                            if(fail){
-                                return error(fail)
-                            }
-            
-                            data = data.json;
-
-                            if(typeof data !== "object" || !data.username || !data.password || !data.email){
-                                return error(2)
-                            }
-
-                            if(!/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(data.email)){
-                                return error(46)
-                            }
-
-                            if(!/^(?![0-9])[a-zA-Z0-9_.-]{2,60}$/.test(data.username)){
-                                return error(47)
-                            }
-
-                            if(!/^(?=.*[^a-zA-Z]).{8,}$/.test(data.password)){
-                                return error(48)
-                            }
-
-                            backend.user.createAccount(data, (err, token)=>{
-                                if(err) {
-                                    return error(err)
+                        await new Promise(resolve => {
+                            req.parseBody((data, fail) => {
+                                if(fail){
+                                    return error(fail)
                                 }
+                
+                                data = data.json;
+    
+                                if(typeof data !== "object" || !data.username || !data.password || !data.email){
+                                    return error(2)
+                                }
+    
+                                if(!/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(data.email)){
+                                    return error(46)
+                                }
+    
+                                if(!/^(?![0-9])[a-zA-Z0-9_.-]{2,60}$/.test(data.username)){
+                                    return error(47)
+                                }
+    
+                                if(!/^(?=.*[^a-zA-Z]).{8,}$/.test(data.password)){
+                                    return error(48)
+                                }
+    
+                                backend.user.createAccount(data, (err, token)=>{
+                                    if(err) {
+                                        return error(err)
+                                    }
+    
+                                    token.success = true;
+                                    
+                                    res.send(token, {
+                                        'Access-Control-Allow-Origin': req.getHeader("origin")
+                                    })
 
-                                token.success = true;
-
-                                res.send(token, {
-                                    'Access-Control-Allow-Origin': req.getHeader("origin")
-                                })
-                            }, req.ip)
-                        }).data()
+                                    resolve()
+                                }, req.ip)
+                            }).data()
+                        })
                     break;
 
                     default:
