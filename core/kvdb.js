34209d32691f60baea827306b7b5a18143698ac8
the-lstv <lstv@mail.com>
2025-01-27 11:11:58 +0100

    make kvdb.js no longer strictly depend on lmdb (well migrate from lmdb soon)

--- core/kvdb.js
+++ core/kvdb.js
@@ -8,11 +8,18 @@
     Description: Key-value database storage module built for Akeno
 */
 
-const lmdb = require('node-lmdb');
+let lmdb;
+
+try {
+    lmdb = require('node-lmdb');
+} catch (e) {
+    console.warn('Warning: node-lmdb module is not installed. Since we are stepping away from this module for countless issues, it is not required, but the database will be switched to memory-only mode.\n* Data will not be stored to disk! *');
+    lmdb = null;
+}
 
 class KeyStorage {
     constructor(path, options){
-        this.env = new lmdb.Env();
+        if(lmdb) this.env = new lmdb.Env();
         this.path = path;
 
         this.timeout = null;
@@ -26,6 +33,8 @@ class KeyStorage {
     }
 
     open(options){
+        if(!lmdb) return this;
+
         this.env.open({
             maxDbs: 3,
             mapSize: 2 * 1024 * 1024 * 1024,
@@ -37,6 +46,8 @@ class KeyStorage {
     }
 
     openDbi(name, options, memoryCache = false){
+        if(!lmdb) return new dbi(this, null, true);
+
         if(this.dbi[name]) return this.dbi[name];
 
         return this.dbi[name] = new dbi(this, this.env.openDbi({
@@ -51,14 +62,23 @@ class KeyStorage {
     }
 
     beginTxn(){
+        if(!lmdb) return null;
         return this.env.beginTxn();
     }
+
+    /**
+     * @deprecated
+     */
 
     queuePendingOperation(){
         if(this.timeout) return;
 
         this.timeout = setTimeout(() => this.commit(), 5);
     }
+
+    /**
+     * @deprecated
+    */
 
     abortPendingWrite(){
         if(this.timeout){
@@ -73,7 +93,7 @@ class KeyStorage {
     }
 
     commit(txn = this.txn){
-        if(!txn) return;
+        if(!txn || !lmdb) return;
 
         try {
             txn.commit();
@@ -99,28 +119,31 @@ class dbi {
     }
 
     beginTxn(){
+        if(!lmdb) return null;
         return this.env.beginTxn();
     }
 
     set(txn, key, value){
-        switch (true) {
-            case value instanceof Buffer:
-                txn.putBinary(this.dbi, key, value);
-                break;
-            case typeof value === "boolean":
-                txn.putBoolean(this.dbi, key, value);
-                break;
-            case typeof value === "string":
-                txn.putString(this.dbi, key, value);
-                break;
-            case typeof value === "object" || Array.isArray(value):
-                txn.putString(this.dbi, key, JSON.stringify(value));
-                break;
-            case typeof value === "number":
-                txn.putNumber(this.dbi, key, value);
-                break;
-            default:
-                throw new Error("Unsupported value type");
+        if(lmdb) {
+            switch (true) {
+                case value instanceof Buffer:
+                    txn.putBinary(this.dbi, key, value);
+                    break;
+                case typeof value === "boolean":
+                    txn.putBoolean(this.dbi, key, value);
+                    break;
+                case typeof value === "string":
+                    txn.putString(this.dbi, key, value);
+                    break;
+                case typeof value === "object" || Array.isArray(value):
+                    txn.putString(this.dbi, key, JSON.stringify(value));
+                    break;
+                case typeof value === "number":
+                    txn.putNumber(this.dbi, key, value);
+                    break;
+                default:
+                    throw new Error("Unsupported value type");
+            }
         }
 
         if(this.memoryCache){
@@ -131,6 +154,7 @@ class dbi {
     }
 
     commitSet(key, value){
+        if(!lmdb) return this.set(null, key, value);
         const txn = this.env.beginTxn();
         try {
             this.set(txn, key, value);
@@ -144,6 +168,7 @@ class dbi {
      */
 
     deferSet(key, value){
+        if(!lmdb) return this.set(null, key, value);
         if(!this.parent.txn) this.parent.txn = this.env.beginTxn();
         this.set(this.parent.txn, key, value);
 
@@ -152,6 +177,8 @@ class dbi {
     }
 
     get(key, type){
+        if(!lmdb) return this.cache.get(key) || null;
+
         if (this.memoryCache && this.cache.has(key)) {
             return this.cache.get(key);
         }
@@ -166,6 +193,8 @@ class dbi {
     }
 
     txnGet(txn, key, type){
+        if(!lmdb) return this.cache.get(key) || null;
+
         switch (type) {
             case "binary": case "buffer": case Buffer:
                 return txn.getBinary(this.dbi, key);
@@ -193,6 +222,11 @@ class dbi {
 
         try {
             for (const key of keys) {
+                if (this.memoryCache && this.cache.has(key)) {
+                    results[key] = this.cache.get(key);
+                    continue;
+                }
+
                 results[key] = this.txnGet(txn, key, type);
             }
 
@@ -204,10 +238,14 @@ class dbi {
 
     delete(txn, key){
         this.cache.delete(key)
+        if(!lmdb) return;
+
         return txn.del(this.dbi, key);
     }
 
     commitDelete(key){
+        if(!lmdb) return this.delete(null, key);
+
         const txn = this.env.beginTxn();
         try {
             this.delete(txn, key);
@@ -217,6 +255,8 @@ class dbi {
     }
 
     deferDelete(key){
+        if(!lmdb) return this.delete(null, key);
+
         if(!this.parent.txn) this.parent.txn = this.env.beginTxn();
         this.delete(this.parent.txn, key);
 
@@ -225,6 +265,8 @@ class dbi {
     }
 
     commit(){
+        if(!lmdb) return;
+
         this.parent.commit();
     }
 
@@ -266,4 +308,4 @@ class dbi {
     }
 }
 
-module.exports = KeyStorage;
+export default KeyStorage;
